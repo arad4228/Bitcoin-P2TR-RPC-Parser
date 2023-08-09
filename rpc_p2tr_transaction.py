413978085310e2e14fa0164f52c5d438639cda00
arad4228 <arad4228@gmail.com>
2023-08-09 20:13:20 +0900

    [KyoungJae[Add One Block P2TR Parser

--- rpc_p2tr_transaction.py
+++ rpc_p2tr_transaction.py
@@ -1,24 +1,27 @@
-# 동작 과정에 대한 흐름.
-# 가장 먼저 블록에서 모든 transaction을 가져온다.
-# 해당 transaction에서 p2tr형식의 주소에서 나가는 transaction인지 check.
-# 확인 방법은 vin의 transaction에 대해서 해당 transaction이 witness_v1_taproot인지 확인
-# 만약 p2tr을 사용하는 transaction에서 taptree를 사용하는 transaction을 고른다.
-# 고르는 방법은 cblock에 대한 정보를 확인한다. (Json Parsing)
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from rpc_config import *
-import pandas as pd
+from rpc_pandas import *
 
 rpc_connection = AuthServiceProxy(f"http://{rpc_user}:{rpc_password}@{rpc_host}:{rpc_port}")
 
-initialBlockNumber = 400000
+# initialBlockNumber = 400000
+initialBlockNumber = 789024
+
+listTxid = []
+listTypes = []
+listFrom = []
+listTo = []
+listURL = []
 
 try:
+    print(f"Current Block: {initialBlockNumber}")
     block_hash = rpc_connection.getblockhash(initialBlockNumber)
     block_info = rpc_connection.getblock(block_hash)
     transactions = block_info['tx']
 
     for transaction in transactions:
         print(f'Reading Transction is {transaction}')
+        url = baseUrl+transaction
         Json_transaction = rpc_connection.getrawtransaction(transaction, 2)
         # if transaction is coinbase, skip transaction next
         if 'coinbase' in Json_transaction['vin'][0]:
@@ -26,15 +29,55 @@ try:
             continue
 
         is_P2TR = False
+        is_TapTree = False
+        # inner transaction from addresses
+        From = []
+
         # if transaction not coinbase, collect all vin transaction.
         for vin in Json_transaction['vin']:
             # check witness_v1_taproot
             utxo = vin['txid']
             index = vin['vout']
             utxo_transaction = rpc_connection.getrawtransaction(utxo, 2)
+            From.append(utxo_transaction['vout'][index]['scriptPubKey']['address'])
+
             # if uxto witness is 'witness_v1_taproot
             if utxo_transaction['vout'][index]['scriptPubKey']['type'] != "witness_v1_taproot":
                 continue
-            
+
+            is_P2TR = True
+
+            # if not taproot spent transaction
+            if len(vin['txinwitness']) == 3:
+                is_TapTree = True
+        
+        # if transaction is not p2tr type
+        if not is_P2TR:
+            # clear From list
+            From.clear()
+            continue
+        
+        # else
+        To = []
+        for vout in Json_transaction['vout']:
+            To.append(vout['scriptPubKey']['address'])
+        
+        Types = []
+        if is_TapTree:
+            Types.append("TapTree")
+        else:
+            Types.append("TapRoot")
+        
+        listTxid.append(transaction)
+        listTypes.append(Types)
+        listFrom.append(From)
+        listTo.append(To)
+        listURL.append(url)
+
+    dataframe = getDataFrame(listTxid, listTypes, listFrom, listTo, listURL)
+    exportDataFrame(dataframe)
+
 except JSONRPCException as e:
     print(f'RPC 호출 Error: {e}')
+    dataframe = getDataFrame(listTxid, listTypes, listFrom, listTo, listURL)
+    exportDataFrame(dataframe)
